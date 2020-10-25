2233ecd8fcb3e238e8310767b8132467b3668e45
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-10-24 22:54:17 -0500

    set up axios get request

--- src/javascripts/components/projectCards.js
+++ src/javascripts/components/projectCards.js
@@ -1,21 +1,23 @@
-const createProjectCards = (arr) => {
+import projectData from '../helpers/projectData';
+
+const createProjectCards = () => {
   let domString = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i].available === true) {
+  projectData.getProjectData().then((response) => {
+    response.forEach((project) => {
       domString += `<div class="box">
-                            <div class="image-fit">
-                                <img src="${arr[i].screenshot}" alt="screenshot of work">
-                            </div>
-                            <div class="inner">
-                                <h3>${arr[i].title}</h3>
-                                <p>${arr[i].description} ${arr[i].technologiesUsed}</p>
-                                <a class="live-button" href="${arr[i].url}">Link</a>
-                                <a class="github-button" href="${arr[i].githubUrl}">GitHub</a>
-                            </div>
-                          </div>`;
-    }
-  }
-  $('#thumbnails').append(domString);
+                      <div class="image-fit">
+                        <img src="${project.screenshot}" alt="screenshot of work">
+                      </div>
+                      <div class="inner">
+                        <h3>${project.title}</h3>
+                        <p>${project.description} ${project.technologiesUsed}</p>
+                        <a class="live-button" href="${project.url}">Link</a>
+                        <a class="github-button" href="${project.githubUrl}">GitHub</a>
+                      </div>
+                    </div>`;
+      $('#thumbnails').append(domString);
+    });
+  });
 };
 
 export default { createProjectCards };
