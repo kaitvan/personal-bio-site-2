34c06a2fee7fdd78f0d87131d0abe7f5c5655212
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-10-24 23:08:44 -0500

    added screenshots

--- src/javascripts/components/projectCards.js
+++ src/javascripts/components/projectCards.js
@@ -1,21 +1,19 @@
 import projectData from '../helpers/projectData';
 
 const createProjectCards = () => {
-  let domString = '';
   projectData.getProjectData().then((response) => {
     response.forEach((project) => {
-      domString += `<div class="box">
-                      <div class="image-fit">
-                        <img src="${project.screenshot}" alt="screenshot of work">
-                      </div>
-                      <div class="inner">
-                        <h3>${project.title}</h3>
-                        <p>${project.description} ${project.technologiesUsed}</p>
-                        <a class="live-button" href="${project.url}">Link</a>
-                        <a class="github-button" href="${project.githubUrl}">GitHub</a>
-                      </div>
-                    </div>`;
-      $('#thumbnails').append(domString);
+      $('#thumbnails').append(`<div class="box" id="printed with forEach">
+                                <div class="image-fit">
+                                  <img src="${project.screenshot}" alt="screenshot of work">
+                                </div>
+                                <div class="inner">
+                                  <h3>${project.title}</h3>
+                                  <p>${project.description} ${project.technologiesUsed}</p>
+                                  <a class="live-button" href="${project.url}">Link</a>
+                                  <a class="github-button" href="${project.githubUrl}">GitHub</a>
+                                </div>
+                              </div>`);
     });
   });
 };
